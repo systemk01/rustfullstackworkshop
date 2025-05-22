c05b61420408633399cc27952cd7fb5668eb694a
systemk01 <systemk1@icloud.com>
2025-05-22 17:25:49 +0200

    serve static files

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -1,4 +1,5 @@
-use actix_web::web::ServiceConfig;
+use actix_files::Files;
+use actix_web::web::{self, ServiceConfig};
 use shuttle_actix_web::ShuttleActixWeb;
 use shuttle_runtime::CustomError;
 use sqlx::Executor;
@@ -20,9 +21,14 @@ async fn actix_web(
     let film_repository = actix_web::web::Data::new(film_repository);
 
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.app_data(film_repository)
+        cfg.service(
+            web::scope("/api")
+            .app_data(film_repository)
             .configure(api_lib::health::service)
-            .configure(api_lib::films::service::<api_lib::film_repository::PostgresFilmRepository>);
+            .configure(api_lib::films::service::<api_lib::film_repository::PostgresFilmRepository>,
+            ),
+        )
+        .service(Files::new("/", "api/shuttle/static").index_file("index.html"));
     };
 
     Ok(config.into())
